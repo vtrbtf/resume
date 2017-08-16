6ebab850ad7c5413c903a46ed4083f08f0d5ca44
Vitor <vitor.tfreitas@gmail.com>
2017-08-16 17:27:20 -0300

    Adding linter check per file

--- linters.js
+++ linters.js
@@ -2,9 +2,15 @@ const fs = require('fs')
 const alex = require('alex')
 const cliches = require('no-cliches');
 
-let text = fs.readFileSync('RESUME.md', 'utf8')
-console.log("Running Alex for catching insensitive, inconsiderate writing")
-console.log(alex.markdown(text).messages)
-console.log("=========================================================================")
-console.log("Running Cliches for catching cliches ;)")
-console.log(cliches(text))
+let check = (filename) => {
+    let text = fs.readFileSync(filename, 'utf8')
+    console.log(`Checking resume: ${filename}`)
+    console.log("Running Alex for catching insensitive, inconsiderate writing")
+    console.log(alex.markdown(text).messages)
+    console.log("=========================================================================")
+    console.log("Running Cliches for catching cliches ;)")
+    console.log(cliches(text))
+    console.log("=========================================================================")
+}
+
+process.argv.slice(2).forEach( e => check(e) )
